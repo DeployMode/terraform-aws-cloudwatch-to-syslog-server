ca0a4fcb12da1d9380b0c7a3404cfd6569f7132a
Joe Niland <joe@originalmind.com.au>
2020-06-25 12:48:52 +1000

    add support for multiple log groups
    upgrade to 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,10 @@
-output log_group {
-  value = "${var.log_group}"
-
-  description = <<EOF
-The name of the log group that is subscribed to. Its log events are forwarded to the syslog server.
-EOF
+output "log_groups" {
+  value       = var.log_groups
+  description = "The name of the log groups that Lambda is subscribed to. Its log events are forwarded to the syslog server."
 }
 
-output lambda_arn {
-  value       = "${aws_lambda_function.cloudwatch_to_syslog_server.arn}"
+output "lambda_arn" {
+  value       = aws_lambda_function.cloudwatch_to_syslog_server.arn
   description = "The ARN of the lambda function subscribed to the log group."
 }
+
